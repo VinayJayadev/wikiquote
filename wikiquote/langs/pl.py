5e4c422f06157d1929645e05eaa7b705b99b175b
Vinay Jayadev <43932798+VinayJayadev@users.noreply.github.com>
2024-03-14 12:43:50 +0100

    Quote of the day retreival error resolved

--- wikiquote/langs/pl.py
+++ wikiquote/langs/pl.py
@@ -1,11 +1,12 @@
 from typing import List, Text, Tuple
-
 import lxml
 
 from .. import utils
 
 MAIN_PAGE = "Strona_główna"
 
+class MissingQOTDException(Exception):
+    pass
 
 def extract_quotes(tree: lxml.html.HtmlElement, max_quotes: int) -> List[Text]:
     q_lst = utils.extract_quotes_li(tree, max_quotes)
@@ -13,10 +14,9 @@ def extract_quotes(tree: lxml.html.HtmlElement, max_quotes: int) -> List[Text]:
 
 
 def qotd(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
-    qotd_title = html_tree.xpath('.//div[text()="Cytat dnia"]')[0]
-    qotd_element = qotd_title.getnext()
-    qotd_components = qotd_element.xpath("table/tbody/tr")
 
-    quote = qotd_components[0].text_content().strip()
-    author = qotd_components[1].text_content().strip()
-    return quote, author
+  quote_element = html_tree.xpath("//div[@class='tresc']//table//tr[1]//td//i//b")[0]
+  quote = quote_element.text_content().strip()
+  author_element = html_tree.xpath("//div[@class='tresc']//table//tr[2]//td//a")[0]
+  author = author_element.text_content().strip()
+  return  quote, author
