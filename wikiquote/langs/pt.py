56d2db035816a8b653d88d8905f24f50bddd19b4
Vinay Jayadev <43932798+VinayJayadev@users.noreply.github.com>
2024-03-14 12:42:42 +0100

    Quote of the day retrieval error resolved

--- wikiquote/langs/pt.py
+++ wikiquote/langs/pt.py
@@ -1,7 +1,7 @@
 from typing import List, Text, Tuple
 
 import lxml
-
+import lxml.html
 from .. import utils
 
 MAIN_PAGE = "Página_principal"
@@ -17,12 +17,20 @@ def extract_quotes(tree: lxml.html.HtmlElement, max_quotes: int) -> List[Text]:
 
     return utils.extract_quotes_li(tree, max_quotes, HEADINGS, WORD_BLOCKLIST)
 
-
 def qotd(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
-    tree = html_tree.get_element_by_id("mf-cdd")
-    quote_author = tree.xpath(".//td")[2].text_content().split("-")
 
-    quote = "-".join(quote_author[:-1]).strip().strip('"')
-    author = quote_author[-1].strip()
+    quote_div = html_tree.xpath("(//div[@class='inhalt'])[1]//div[@style='background: #fff5f5; padding-top: 0.4em; padding-bottom: 0.3em; text-align:center;']")[0]
+    quote_text = quote_div.xpath(".//p//b//text()")[0].strip()
+    author_text = quote_div.xpath(".//p/a/text()")[-1].strip()
 
-    return quote, author
+    return quote_text, author_text
+
+
+
+
+
+
+
+
+
+
